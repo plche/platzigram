f7c0d7c46a5b57e8fe17fa1f0823ba477e792393
plche <che.percy.l@gmail.com>
2022-06-02 21:55:07 -0500

    Post model registration in Django's admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,34 @@
+"""Post admin classes."""
+
+# Django
 from django.contrib import admin
 
+# Models
+from posts.models import Post
+
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+
+    list_display = ('pk', 'user', 'profile', 'title', 'photo')
+    list_display_links = ('pk', 'user')
+    list_editable = ('title', 'photo')
+
+    search_fields = ('title',)
+
+    list_filter = ('created', 'modified')
+
+    fieldsets = (
+        ('Post', {
+            'fields': (('pk', 'title'),),
+        }),
+        ('Extra info', {
+            'fields': (('user', 'profile'),),
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'),),
+        }),
+    )
